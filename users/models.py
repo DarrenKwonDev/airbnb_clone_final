9556cd2f12adece74183e62cf4ea3a5cdd5ab196
DarrenKwonDev <anatomy1545@gmail.com>
2020-06-11 16:16:21 +0900

    user model and custom admin panel

---
+++ users/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "ko"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USE"),
+        (CURRENCY_KRW, "KRW"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES,
+                              max_length=10, null=True, blank=True)
+    bio = models.TextField(default="")
+    birthdate = models.DateField(null=True, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=10, null=True, blank=True)
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=10, null=True, blank=True)
+    superhost = models.BooleanField(default=False)
