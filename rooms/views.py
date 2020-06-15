68b7389e91ef2e4380e3709e37fbe9b86a9f5f2c
DarrenKwonDev <anatomy1545@gmail.com>
2020-06-15 18:33:47 +0900

    create pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,16 @@
+from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 # Create your views here.
-def all_rooms(req):
-    all_rooms = models.Room.objects.all()
-    return render(req, "rooms/home.html", context={"rooms": all_rooms})
+def all_rooms(request):
+    page = int(request.GET.get("page"))
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, per_page=10,)
+    rooms = paginator.get_page(page)
+
+    print(vars(rooms))
+    print(vars(rooms.paginator))
+
+    return render(request, "rooms/home.html", {"rooms": rooms},)
