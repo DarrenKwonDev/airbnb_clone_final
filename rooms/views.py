331d7752dc8b45ba56087de5e8e632879b657f94
DarrenKwonDev <anatomy1545@gmail.com>
2020-06-16 02:42:10 +0900

    change to class based view

--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,36 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.views.generic import ListView
+from django.utils import timezone
 from . import models
 
-# Create your views here.
-def all_rooms(request):
-    page = int(request.GET.get("page"))
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, per_page=10,)
-    rooms = paginator.get_page(page)
 
-    print(vars(rooms))
-    print(vars(rooms.paginator))
+class HomeView(ListView):
 
-    return render(request, "rooms/home.html", {"rooms": rooms},)
+    model = models.Room
+
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
+
+
+# from django.shortcuts import render, redirect
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+# # Create your views here.
+# def all_rooms(request):
+#     page = request.GET.get("page", 1)
+#     room_list = models.Room.objects.all()
+#     paginator = Paginator(room_list, per_page=10, orphans=5)
+
+#     try:
+#         rooms = paginator.page(int(page))
+#         return render(request, "rooms/home.html", {"rooms": rooms},)
+
+#     except Exception:
+#         return redirect("/")
