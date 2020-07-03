f63818b938fa22c5dd494f09cd08c8e9e73878ac
DarrenKwonDev <anatomy1545@gmail.com>
2020-07-03 10:26:51 +0900

    serach form

--- rooms/views.py
+++ rooms/views.py
@@ -30,11 +30,48 @@ class RoomDetail(DetailView):
 
 
 def search(request):
-    print(request.GET)
+
+    # 유저가 입력한 값을 url에서 가져옴 why? 입력한 후에도 value로 남겨주려고
     city = str.capitalize(request.GET.get("city", "Anywhere"))
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+
+    print(s_amenities, s_facilities)
+
+    form = {
+        "city": city,
+        "s_country": country,
+        "s_room_type": room_type,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": super_host,
+    }
+
+    # 유저가 select 할 수 있도록 정보를 가져옴
+
     room_types = models.RoomType.objects.all()
-    return render(
-        request,
-        "rooms/search.html",
-        {"city": city, "countries": countries, "room_types": room_types},
-    )
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+    return render(request, "rooms/search.html", {**form, **choices})
